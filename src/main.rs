55c60748de80d6ffc815dbb66961bc530b10b901
Pratik Gorade <pratikgorade0@gmail.com>
2021-01-13 13:30:14 +0530

    impl UserDB: add_user now returns true if user added, false if user already present wrote tests for adding users
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,16 @@ impl UserDB {
         }
     }
 
-    fn add_user(&mut self, username: String, password: String) {
-        self.db
-            .lock()
-            .unwrap()
-            .insert(username, bcrypt::hash(password, 4).unwrap());
+    fn add_user(&mut self, username: String, password: String) -> bool {
+        if !self.db.lock().unwrap().contains_key(&username) {
+            self.db
+                .lock()
+                .unwrap()
+                .insert(username, bcrypt::hash(password, 4).unwrap());
+            true
+        } else {
+            false
+        }
     }
 
     fn verify_user(&self, username: String, password: String) -> bool {
@@ -35,27 +40,40 @@ fn main() {
 }
 
 #[cfg(test)]
-mod tests {
+mod db_tests {
     use super::*;
 
     fn create_dummy_user() -> UserDB {
         let mut new_user_db = UserDB::new();
-        new_user_db.add_user(String::from("abcd"), String::from("abcd"));
+        new_user_db.add_user(String::from("username"), String::from("password"));
         new_user_db
     }
 
     #[test]
-    fn verify_correct_user() {
-        assert!(create_dummy_user().verify_user(String::from("abcd"), String::from("abcd")));
+    fn verify_with_correct_username_and_password() {
+        assert!(create_dummy_user().verify_user(String::from("username"), String::from("password")));
     }
 
     #[test]
-    fn no_user() {
-        assert!(!UserDB::new().verify_user(String::from("abcde"), String::from("abcd")));
+    fn verify_with_wrong_username() {
+        assert!(
+            !UserDB::new().verify_user(String::from("wrong_username"), String::from("password"))
+        );
     }
 
     #[test]
-    fn wrong_password() {
-        assert!(!create_dummy_user().verify_user(String::from("abcd"), String::from("abcde")));
+    fn verify_with_wrong_password() {
+        assert!(!create_dummy_user()
+            .verify_user(String::from("username"), String::from("wrong_password")));
+    }
+
+    #[test]
+    fn add_user() {
+        assert!(UserDB::new().add_user(String::from("username"), String::from("password")));
+    }
+
+    #[test]
+    fn add_duplicate_user() {
+        assert!(!create_dummy_user().add_user(String::from("username"), String::from("password")));
     }
 }
